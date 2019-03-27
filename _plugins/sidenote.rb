2a5fd9a5ded80a96cf67cd70fb324cb9aaad1619
Dr Kim Foale <kim@gfsc.studio>
2019-03-27 19:04:44 +0000

    Updates

--- _plugins/sidenote.rb
+++ _plugins/sidenote.rb
@@ -2,7 +2,7 @@ module Jekyll
   class SidenoteBlock < Liquid::Block
     def render(context)
       text = super
-      "<div class='sidenote'>#{text}</div>"
+      "<div class='sidenote' markdown='1'>#{text}</div>"
     end
   end
 end
