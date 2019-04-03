5dbb8316726615bf46041f45ce75be2b7e4db984
Dr Kim Foale <kim@gfsc.studio>
2019-04-03 12:36:33 +0100

    Homepage and responsive big revamp

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function() {
+  var hamburger = {
+    toggle: document.getElementById('js-toggle'),
+    nav: document.getElementById('js-nav'),
+    doToggle: function(e) {
+     e.preventDefault();
+     this.toggle.classList.toggle('expanded');
+     this.nav.classList.toggle('expanded');
+    }
+  }
+
+  hamburger.toggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+}());
